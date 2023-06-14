16bc634dd3b2d5e4aec545428c735b59d873e5f1
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2023-06-14 11:02:08 -0400

    Update mmseqs2_annotation.sh

--- mmseqs2_annotation.sh
+++ mmseqs2_annotation.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 ### MMSeqs2 Annotation ###
 # This script uses MMSeqs2 to attribute UniRef100 ids to the sequences in the fasta file
 
+# Terminology:
+    # Query: the fasta file to be annotated
+    # Target: the database to be searched against
+    # Result: the output of the search
+
 # Edit this to your project directory
 PROJ_DIR="/mnt/ssd/elliot"
 
@@ -12,15 +17,17 @@ mdkir "$PROJ_DIR"
 mdkir "$PROJ_DIR"/tmp
 
 
-# First, download the UniRef100 database
-mmseqs databases UniRef100 "$PROJ_DIR"/uniref100 "$PROJ_DIR"/tmp
+# First, download the UniRef100 database to be used as target
+mmseqs databases UniRef100 "$PROJ_DIR"/uniref100 "$PROJ_DIR"/tmp --threads 8
+# => Donne une seqTaxDB, lire https://github.com/soedinglab/mmseqs2/wiki#terminology
 
 # Then, create sequence database for query and target
-mmseqs createdb "$PROJ_DIR"/uniref100 "$PROJ_DIR"/uniref100DB
 mmseqs createdb "$PROJ_DIR"/panproteome-cacnes.fasta "$PROJ_DIR"/panproteome-cacnesDB
+# mmseqs createdb "$PROJ_DIR"/uniref100 "$PROJ_DIR"/uniref100DB 
+#   Only if trying to make targetDB from fasta file. Otherwise, just use mmseqs databases
 
 # Then, create index for target database
-mmseqs createindex "$PROJ_DIR"/panproteome-cacnesDB "$PROJ_DIR"/tmp
+mmseqs createindex "$PROJ_DIR"/uniref100 "$PROJ_DIR"/tmp --threads 32
 
 # Then, search the query database against the target database
     # mmseqs search queryDB targetDB resultDB tmp
